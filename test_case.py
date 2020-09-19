eec02eb91acc978d00f87cc4f85c8ff1a8a168e0
seryte <382643558@qq.com>
2020-09-19 21:49:28 +0800

    async-demo

--- test_case.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def login():
-	payload = {"username":"admin","password":"introcks" }
-	r = requests.Session().post("http://192.168.11.59:3000/login",data=payload)
-	print(r)
-
-
-login()
